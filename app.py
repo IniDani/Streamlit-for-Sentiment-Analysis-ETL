990e3c2890ff03e68a23269ce2dd510f363a2a28
IniDani <msaarafli007@gmail.com>
2024-11-21 12:10:17 +0700

    feat: add a way to connect to db (doesn't work currently)

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine
+from Credentials import (
+    POSTGRESQL_HOST,
+    POSTGRESQL_PORT,
+    POSTGRESQL_USER,
+    POSTGRESQL_PASSWORD,
+    POSTGRESQL_DATABASE
+)
 
 # Database Connection
-connection_string = "postgresql://DanFaRa@rekdatsentimenanalysis:Qwerty123@rekdatsentimenanalysis.postgres.database.azure.com:5432/sentiment_analysis_etl?sslmode=require"
-engine = create_engine(connection_string)
+db_config = {
+    "username": POSTGRESQL_USER,
+    "password": POSTGRESQL_PASSWORD,
+    "host": POSTGRESQL_HOST,
+    "port": POSTGRESQL_PORT,
+    "database": POSTGRESQL_DATABASE
+}
+# connection_string = "postgresql://DanFaRa@rekdatsentimenanalysis:Qwerty123@rekdatsentimenanalysis.postgres.database.azure.com:5432/sentiment_analysis_etl?sslmode=require"
+# engine = create_engine(connection_string)
+
+# Create the database URL
+db_url = f"postgresql+psycopg2://{db_config['username']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}?sslmode=require"
+
+# Create the SQLAlchemy engine
+engine = create_engine(db_url, pool_pre_ping = True)
 
 # Streamlit App
 st.title("Sentiment Analysis Data Viewer")
